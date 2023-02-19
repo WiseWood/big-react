7551aef39b8c233d34ef933e0719047430061127
林伟 <lin.wei126@iwhalecloud.com>
2023-02-19 13:44:04 +0800

    feat: [4-1] 实现状态更新机制 1、​代表更新的数据结构 Update 2、​消费update的数据结构 UpdateQueue

---
+++ packages/react-reconciler/src/updateQueue.ts
@@ -0,0 +1,60 @@
+import { Action } from 'shared/ReactTypes';
+
+export interface Update<State> {
+	action: Action<State>;
+}
+
+export interface UpdateQueue<State> {
+	shared: {
+		pending: Update<State> | null;
+	};
+}
+
+// 创建更新
+export const createUpdate = <State>(action: Action<State>): Update<State> => {
+	return {
+		action
+	};
+};
+
+// 创建更新队列
+export const createUpdateQueue = <State>() => {
+	return {
+		shared: {
+			pending: null
+		}
+	} as UpdateQueue<State>;
+};
+
+// 更新入列
+export const enqueueUpdate = <State>(
+	updateQueue: UpdateQueue<State>,
+	update: Update<State>
+) => {
+	updateQueue.shared.pending = update;
+};
+
+// 处理更新
+export const processUpdateQueue = <State>(
+	baseState: State,
+	pendingUpdate: Update<State> | null
+): { memoizedState: State } => {
+	const result: ReturnType<typeof processUpdateQueue<State>> = {
+		memoizedState: baseState
+	};
+
+	if (pendingUpdate !== null) {
+		const action = pendingUpdate.action;
+		if (action instanceof Function) {
+			// baseState:1   update ->  (x) => 4x  -> memoizedState:4
+			// 例如：this.setState((x) => 4x)
+			result.memoizedState = action(baseState);
+		} else {
+			// baseState:1   update ->  2  -> memoizedState:2
+			// 例如：this.setState(2)
+			result.memoizedState = action;
+		}
+	}
+
+	return result;
+};
