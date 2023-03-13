df3dc9ff65dc9c0558c0611c3277bad29cbf32e6
林伟 <lin.wei126@iwhalecloud.com>
2023-03-13 23:16:04 +0800

    feat: [8-2] 实现mount流程的useState 一、定义变量 1、记录当前渲染的函数组件的fiber 2、记录当前渲染的函数组件的hooks链表 二、mount时hooks集合之useState 1、创建 hook，并建立 hook 链表链接，记录在 fiberNode memoizedState 上 2、创建 updateQueue，记录在hook 上 3、dispatch 接入更新机制，记录在 updateQueue 上

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
+	const [num] = useState(100);
 	return (
 		<div>
-			<Child />
+			{/* <Child /> */}
+			{num}
 		</div>
 	);
 }
--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,12 +1,33 @@
 import internals from 'shared/internals';
-import { Dispatcher } from 'react/src/currentDispatcher';
+import { Dispatch, Dispatcher } from 'react/src/currentDispatcher';
 import { FiberNode } from './fiber';
+import {
+	createUpdate,
+	createUpdateQueue,
+	enqueueUpdate,
+	UpdateQueue
+} from './updateQueue';
+import { scheduleUpdateOnFiber } from './workLoop';
+import { Action } from 'shared/ReactTypes';
 
 const { currentDispatcher } = internals;
 
-export function renderWithHooks(wip: FiberNode) {
-	const current = wip.alternate;
+let currentlyRenderingFiber: FiberNode | null = null;
+let workInProgressHook: Hook | null = null;
 
+interface Hook {
+	memoizedState: any;
+	updateQueue: unknown;
+	next: Hook | null;
+}
+
+export function renderWithHooks(wip: FiberNode) {
+	// 记录当前函数组件render对应的 fiber
+	currentlyRenderingFiber = wip;
+	// 重置
+	wip.memoizedState = null;
+
+	const current = wip.alternate;
 	if (current !== null) {
 		// update
 	} else {
@@ -18,6 +39,9 @@ export function renderWithHooks(wip: FiberNode) {
 	const props = wip.pendingProps;
 	const children = Component(props);
 
+	// 重置操作
+	currentlyRenderingFiber = null;
+
 	return children;
 }
 
@@ -26,6 +50,63 @@ const HooksDispatcherOnMount: Dispatcher = {
 	useState: mountState
 };
 
-function mountState<State>(initialState: (() => State) | State): any {
-	return;
+function mountState<State>(
+	initialState: (() => State) | State
+): [State, Dispatch<State>] {
+	// 创建 hook，并建立 hook 链表链接
+	const hook = mountWorkInProgressHook();
+
+	let memoizedState;
+	if (initialState instanceof Function) {
+		memoizedState = initialState();
+	} else {
+		memoizedState = initialState;
+	}
+
+	const queue = createUpdateQueue<State>();
+	// 记录 updateQueue，供之后 update 使用
+	hook.updateQueue = queue;
+	hook.memoizedState = memoizedState;
+
+	// @ts-ignore
+	const dispatch = dispatchSetState.bind(null, currentlyRenderingFiber, queue);
+	// dispatch 记录在 queue 中，供之后 update 使用
+	queue.dispatch = dispatch;
+
+	return [memoizedState, dispatch];
+}
+
+function dispatchSetState<State>(
+	fiber: FiberNode,
+	updateQueue: UpdateQueue<State>,
+	action: Action<State>
+) {
+	// 接入更新机制
+	const update = createUpdate(action);
+	enqueueUpdate(updateQueue, update);
+	scheduleUpdateOnFiber(fiber);
+}
+
+function mountWorkInProgressHook(): Hook {
+	const hook: Hook = {
+		memoizedState: null,
+		updateQueue: null,
+		next: null
+	};
+
+	// mount时，第一个 hook
+	if (workInProgressHook === null) {
+		if (currentlyRenderingFiber === null) {
+			throw new Error('请在函数组件内调用hook');
+		} else {
+			workInProgressHook = hook;
+			currentlyRenderingFiber.memoizedState = workInProgressHook; // 记录当前函数组件的 hooks
+		}
+	} else {
+		// mount时，后面的 hook，构成单向链表
+		workInProgressHook.next = hook;
+		workInProgressHook = hook;
+	}
+
+	return workInProgressHook;
 }
