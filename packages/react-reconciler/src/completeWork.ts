44006d64257c4222006724aeee33eacf685cb087
林伟 <lin.wei126@iwhalecloud.com>
2023-03-18 16:48:19 +0800

    feat: [10-1] 实现update流程的completeWork部分 1、暂时只处理 HostText

--- packages/react-reconciler/src/completeWork.ts
+++ packages/react-reconciler/src/completeWork.ts
@@ -5,13 +5,17 @@ import {
 	createTextInstance
 } from 'hostConfig';
 import { FiberNode } from './fiber';
-import { NoFlags } from './fiberFlags';
+import { NoFlags, Update } from './fiberFlags';
 import {
 	FunctionComponent,
 	HostComponent,
 	HostRoot,
 	HostText
 } from './workTags';
+
+function markUpdate(fiber: FiberNode) {
+	fiber.flags |= Update;
+}
 
 // 递归中的归阶段做的事
 export const completeWork = (wip: FiberNode) => {
@@ -34,6 +38,12 @@ export const completeWork = (wip: FiberNode) => {
 		case HostText:
 			if (current !== null && wip.stateNode) {
 				// update
+				const oldText = current.memoizedProps.content;
+				const newText = newProps.content;
+
+				if (oldText !== newText) {
+					markUpdate(wip);
+				}
 			} else {
 				// 1. 构建DOM
 				const instance = createTextInstance(newProps.content);
